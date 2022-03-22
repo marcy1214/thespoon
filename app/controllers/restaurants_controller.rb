4308c0f6de505ea0da9a04180af12c4439570b65
Marcy Hundschell Dela Cruz <90773594+marcy1214@users.noreply.github.com>
2022-03-22 14:25:48 +0100

    finish crud actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,12 +12,32 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
   end
 end
