57c5c885ded0f3b9d634c3cc4348859d58db00ba
Marcy Hundschell Dela Cruz <90773594+marcy1214@users.noreply.github.com>
2022-03-26 07:04:26 -0700

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
 100.times do
   Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_name,
     rating: rand(1..5)
   )
+  puts "restaurant" # {restaurant.id} is created"
 end
+puts "Finished!"
